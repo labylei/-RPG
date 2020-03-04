b3564a285f28425382ff09308b10f9a43ad70bb8
labylei <57024733+labylei@users.noreply.github.com>
2020-03-04 11:23:19 +0800

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Engine.ViewModels;
+using Engine.EventArgs;
+using Engine.Models;
+namespace WPFUI
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly GameSession _gameSession=new GameSession();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _gameSession.OnMessageRaised += OnGameMessageRaised;
+
+            DataContext = _gameSession;//把GameSession连接到UI上，初始化就可以用GameSession类的数据进行绑定
+        }
+
+        private void OnClick_MoveNorth(object sender,RoutedEventArgs e)
+        {
+            _gameSession.MoveNorth();
+        }
+        private void OnClick_MoveWest(object sender,RoutedEventArgs e)
+        {
+            _gameSession.MoveWest();
+        }
+        private void OnClick_MoveEast(object sender,RoutedEventArgs e)
+        {
+            _gameSession.MoveEast();
+        }
+        private void OnClick_MoveSouth(object sender,RoutedEventArgs e)
+        {
+            _gameSession.MoveSouth();
+        }
+
+        private void OnClick_AttackMonster(object sender,RoutedEventArgs e)
+        {
+            _gameSession.AttackCurrentMonster();
+        }
+
+        private void OnClick_UseCurrentConsumable(object sender, RoutedEventArgs e)
+        {
+            _gameSession.UseCurrentConsumable();
+        }
+
+        private void OnGameMessageRaised(object sender,GameMessageEventArgs e)
+        {
+            GameMessages.Document.Blocks.Add(new Paragraph(new Run(e.Message)));
+            GameMessages.ScrollToEnd();
+        }
+
+        private void OnClick_DisplayTradeScreen(object sender,RoutedEventArgs e)
+        {
+            TradeScreen tradeScreen = new TradeScreen();
+            tradeScreen.Owner = this;
+            tradeScreen.DataContext = _gameSession;
+            tradeScreen.ShowDialog();
+        }
+
+       private void OnClick_Craft(object sender,RoutedEventArgs e)
+        {
+            Recipe recipe = ((FrameworkElement)sender).DataContext as Recipe;
+            _gameSession.CreaftItemUsing(recipe);
+        }
+    }
+}
---
+++ TradeScreen.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Engine.Models;
+using Engine.ViewModels;
+namespace WPFUI
+{
+    /// <summary>
+    /// TradeScreen.xaml 的交互逻辑
+    /// </summary>
+    public partial class TradeScreen : Window
+    {
+        public GameSession Session => DataContext as GameSession;
+        public TradeScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void OnClick_Sell(object sender,RoutedEventArgs e)
+        {
+            GroupedInventoryItem groupedInventoryItem = ((FrameworkElement)sender).DataContext as GroupedInventoryItem;
+
+            if(groupedInventoryItem != null)
+            {
+                Session.CurrentPlayer.ReceiveGold(groupedInventoryItem.Item.Price);
+                Session.CurrentTrader.AddItemToInventory(groupedInventoryItem.Item);
+                Session.CurrentPlayer.RemoveItemFromInventory(groupedInventoryItem.Item);
+            }
+        }
+        private void OnClick_Buy(object sender,RoutedEventArgs e)
+        {
+            GroupedInventoryItem groupedInventoryItem = ((FrameworkElement)sender).DataContext as GroupedInventoryItem;
+
+            if(groupedInventoryItem != null)
+            {
+                if (Session.CurrentPlayer.Gold >= groupedInventoryItem.Item.Price)
+                {
+                    Session.CurrentPlayer.SpendGold(groupedInventoryItem.Item.Price);
+                    Session.CurrentPlayer.AddItemToInventory(groupedInventoryItem.Item);
+                    Session.CurrentTrader.RemoveItemFromInventory(groupedInventoryItem.Item);
+                }
+                else
+                {
+                    MessageBox.Show("你不够钱");
+                }
+            }
+        }
+        private void OnClick_Close(object sender,RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+
+}
